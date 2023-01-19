feb5f4726980176e9f39bdcad65ef759de3b9106
John T Souba <johntsouba@gmail.com>
2023-01-18 23:21:42 -0600

    demo code

---
+++ src/main/java/com/tsg/demo/SpringPostGetDemoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.tsg.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPostGetDemoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPostGetDemoProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tsg/demo/SpringPostGetDemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsg.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPostGetDemoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
